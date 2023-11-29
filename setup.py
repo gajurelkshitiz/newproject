fe93ed7ab72f50dec5611c5079fb7a5d07476711
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2023-11-29 13:31:56 +0545

    setup file added

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup,find_packages
 from typing import List
 
+HYPEN_E_DOT = '-e .'
+
 def get_requirements(file_path: str) -> List[str]:
     # This function will return the lists of requirements.
     
@@ -8,6 +10,9 @@ def get_requirements(file_path: str) -> List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
     return requirements
 
@@ -18,5 +23,5 @@ setup(
     author='Kshitiz Gajurel',
     author_email='kshitiz.purbct042@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirement.txt')
+    install_requires= get_requirements('requirements.txt')
 )
